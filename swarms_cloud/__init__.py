a134107f2c88a0a633185daaba8a6dcac6c258e0
Kye <kye@apacmediasolutions.com>
2023-12-09 10:41:59 -0800

    [CLEANUP]

--- swarms_cloud/__init__.py
+++ swarms_cloud/__init__.py
@@ -1,8 +1,10 @@
 from swarms_cloud.main import agent_api_wrapper
 from swarms_cloud.rate_limiter import rate_limiter
-
+from swarms_cloud.func_api_wrapper import api_wrapper, FuncAPIWrapper
 
 __all__ = [
     "agent_api_wrapper",
     "rate_limiter",
+    "api_wrapper",
+    "FuncAPIWrapper",
 ]
