8da9e6cc7b11beeebb061ba62bdcc2fa5dcbaea7
Kye <kye@apacmediasolutions.com>
2023-12-18 11:09:57 -0500

    [FuncAPIWrapper]

--- swarms_cloud/func_api_wrapper.py
+++ swarms_cloud/func_api_wrapper.py
@@ -9,50 +9,6 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-# Function API Wrapper for functions: [FUNCTION]
-def api_wrapper(
-    app: FastAPI,
-    path: str,
-    http_method: str = "post",
-):
-    """API Wrapper
-
-    Args:
-        app (FastAPI): _description_
-        path (str): _description_
-        http_method (str, optional): _description_. Defaults to "post".
-
-    Example:
-    >>> app = FastAPI()
-    >>> @api_wrapper(app, "/endpoint")
-    ... def endpoint():
-    ...     return "Hello World"
-    >>> uvicorn.run(app, host="")
-    """
-
-    def decorator(func: Callable):
-        try:
-
-            async def endpoint_wrapper(*args, **kwargs):
-                try:
-                    # Call the func with provided args
-                    logger.info(f"Calling method {func.__name__}")
-                    result = func(*args, **kwargs)
-                    return result
-                except Exception as error:
-                    logger.error(f"Error in {func.__name__}: {error}")
-                    raise HTTPException(status_code=500, detail=str(error))
-
-            # Register the endpoint
-            endpoint_func = getattr(app, http_method)
-            endpoint_func(path)(endpoint_wrapper)
-            return func
-        except Exception as error:
-            print(f"Error in {func.__name__}: {error}")
-
-    return decorator
-
-
 # Function API Wrapper for functions: [CLASS]
 class FuncAPIWrapper:
     """Functional API Wrapper
--- tests/test_func_wrapper.py
+++ tests/test_func_wrapper.py
@@ -1,78 +1,166 @@
+from unittest.mock import patch
+
 import pytest
+from fastapi import FastAPI, Request
 from fastapi.testclient import TestClient
+from starlette.middleware.base import BaseHTTPMiddleware
 
-from swarms_cloud.func_api_wrapper import FuncAPIWrapper, api_wrapper
+from swarms_cloud.func_api_wrapper import FuncAPIWrapper
 
 
-# Create an instance of FuncAPIWrapper for testing
+# Create a fixture for an instance of FuncAPIWrapper
 @pytest.fixture
-def api():
+def func_api_wrapper():
     return FuncAPIWrapper()
 
 
-# Create a test client
+# Create a fixture for a mock of the uvicorn module
 @pytest.fixture
-def client(api):
-    return TestClient(api.app)
+def mock_uvicorn():
+    with patch("swarms_cloud.func_api_wrapper.uvicorn") as mock:
+        yield mock
 
 
-# Define some sample functions to test
-def sample_function():
-    return "Hello World"
+def test_init(func_api_wrapper):
+    assert func_api_wrapper.host == "0.0.0.0"
+    assert func_api_wrapper.port == 8000
+    assert isinstance(func_api_wrapper.app, FastAPI)
 
 
-def sample_function_with_error():
-    raise Exception("Test Error")
+def test_add(func_api_wrapper):
+    @func_api_wrapper.add("/test", method="get")
+    def test_endpoint():
+        return {"message": "test"}
 
-
-# Define tests for FuncAPIWrapper
-def test_api_wrapper_decorator(api):
-    @api.add("/endpoint")
-    def endpoint():
-        return "Hello World"
-
-    assert api.app.routes[-1].path == "/endpoint"
-    assert api.app.routes[-1].endpoint.__name__ == "endpoint"
-
-
-def test_func_api_wrapper_run(api, client):
-    @api.add("/endpoint")
-    def endpoint():
-        return "Hello World"
-
-    response = client.get("/endpoint")
+    client = TestClient(func_api_wrapper.app)
+    response = client.get("/test")
     assert response.status_code == 200
-    assert response.text == "Hello World"
+    assert response.json() == {"message": "test"}
 
 
-def test_func_api_wrapper_error_handling(api, client):
-    @api.add("/endpoint")
-    def endpoint():
-        raise Exception("Test Error")
+def test_add_invalid_method(func_api_wrapper):
+    with pytest.raises(ValueError):
 
-    response = client.get("/endpoint")
-    assert response.status_code == 500
+        @func_api_wrapper.add("/test", method="invalid")
+        def test_endpoint():
+            return {"message": "test"}
 
 
-# Define tests for api_wrapper decorator
-def test_api_wrapper_success(client):
-    app = FuncAPIWrapper()
+def test_run(func_api_wrapper, mock_uvicorn):
+    func_api_wrapper.run()
+    mock_uvicorn.run.assert_called_once_with(
+        func_api_wrapper.app, host="0.0.0.0", port=8000
+    )
 
-    @api_wrapper(app.app, "/endpoint")
-    def endpoint():
-        return "Hello World"
 
-    response = client.get("/endpoint")
+def test_call(func_api_wrapper, mock_uvicorn):
+    func_api_wrapper()
+    mock_uvicorn.run.assert_called_once_with(
+        func_api_wrapper.app, host="0.0.0.0", port=8000
+    )
+
+
+def test_add_post(func_api_wrapper):
+    @func_api_wrapper.add("/test_post", method="post")
+    def test_post_endpoint():
+        return {"message": "test_post"}
+
+    client = TestClient(func_api_wrapper.app)
+    response = client.post("/test_post")
     assert response.status_code == 200
-    assert response.text == "Hello World"
+    assert response.json() == {"message": "test_post"}
 
 
-def test_api_wrapper_error_handling(client):
-    app = FuncAPIWrapper()
+def test_add_put(func_api_wrapper):
+    @func_api_wrapper.add("/test_put", method="put")
+    def test_put_endpoint():
+        return {"message": "test_put"}
 
-    @api_wrapper(app.app, "/endpoint")
-    def endpoint():
-        raise Exception("Test Error")
+    client = TestClient(func_api_wrapper.app)
+    response = client.put("/test_put")
+    assert response.status_code == 200
+    assert response.json() == {"message": "test_put"}
 
-    response = client.get("/endpoint")
+
+def test_add_delete(func_api_wrapper):
+    @func_api_wrapper.add("/test_delete", method="delete")
+    def test_delete_endpoint():
+        return {"message": "test_delete"}
+
+    client = TestClient(func_api_wrapper.app)
+    response = client.delete("/test_delete")
+    assert response.status_code == 200
+    assert response.json() == {"message": "test_delete"}
+
+
+def test_error_handling(func_api_wrapper):
+    @func_api_wrapper.add("/test_error", method="get")
+    def test_error_endpoint():
+        raise Exception("Test exception")
+
+    client = TestClient(func_api_wrapper.app)
+    response = client.get("/test_error")
     assert response.status_code == 500
+    assert "Test exception" in response.text
+
+
+def test_rate_limiting(func_api_wrapper):
+    @func_api_wrapper.add("/test_rate_limit", method="get")
+    def test_rate_limit_endpoint():
+        return {"message": "test_rate_limit"}
+
+    client = TestClient(func_api_wrapper.app)
+    for _ in range(6):
+        response = client.get("/test_rate_limit")
+    assert response.status_code == 429
+    assert "Too Many Requests" in response.text
+
+
+# Custom middleware for testing
+class CustomMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next):
+        response = await call_next(request)
+        response.headers["X-Custom-Header"] = "Test"
+        return response
+
+def test_add_patch(func_api_wrapper):
+    @func_api_wrapper.add("/test_patch", method="patch")
+    def test_patch_endpoint():
+        return {"message": "test_patch"}
+
+    client = TestClient(func_api_wrapper.app)
+    response = client.patch("/test_patch")
+    assert response.status_code == 200
+    assert response.json() == {"message": "test_patch"}
+
+def test_request_data(func_api_wrapper):
+    @func_api_wrapper.add("/test_data", method="post")
+    async def test_data_endpoint(request: Request):
+        data = await request.json()
+        return data
+
+    client = TestClient(func_api_wrapper.app)
+    response = client.post("/test_data", json={"key": "value"})
+    assert response.status_code == 200
+    assert response.json() == {"key": "value"}
+
+def test_query_params(func_api_wrapper):
+    @func_api_wrapper.add("/test_params", method="get")
+    def test_params_endpoint(key: str):
+        return {"key": key}
+
+    client = TestClient(func_api_wrapper.app)
+    response = client.get("/test_params?key=value")
+    assert response.status_code == 200
+    assert response.json() == {"key": "value"}
+
+def test_custom_middleware(func_api_wrapper):
+    func_api_wrapper.app.middleware("http")(CustomMiddleware)
+    @func_api_wrapper.add("/test_middleware", method="get")
+    def test_middleware_endpoint():
+        return {"message": "test_middleware"}
+
+    client = TestClient(func_api_wrapper.app)
+    response = client.get("/test_middleware")
+    assert response.status_code == 200
+    assert response.headers["X-Custom-Header"] == "Test"
