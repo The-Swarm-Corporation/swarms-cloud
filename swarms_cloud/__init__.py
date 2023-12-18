86c68961c2cd5f8a92de31858d06805c4229b296
Kye <kye@apacmediasolutions.com>
2023-12-18 10:29:39 -0500

    [__INIT__]

--- swarms_cloud/__init__.py
+++ swarms_cloud/__init__.py
@@ -1,10 +1,14 @@
 from swarms_cloud.main import agent_api_wrapper
 from swarms_cloud.rate_limiter import rate_limiter
 from swarms_cloud.func_api_wrapper import api_wrapper, FuncAPIWrapper
+from swarms_cloud.api_key_generator import generate_api_key
+from swarms_cloud.sky_api import SkyInterface
 
 __all__ = [
     "agent_api_wrapper",
     "rate_limiter",
     "api_wrapper",
     "FuncAPIWrapper",
+    "generate_api_key",
+    "SkyInterface",
 ]
