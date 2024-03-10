6e2100a7969c924b8021a8347e98ab0337ecb9b9
Kye <kye@apacmediasolutions.com>
2024-03-09 22:10:28 -0800

    [CLEANUP]

--- scripts/main.py
+++ scripts/main.py
@@ -20,57 +20,63 @@ qwen_infer = QWenInfer(
     log_level="info",
     output_csv=None,
     output_npy=None,
-    num_beams=1
+    num_beams=1,
 )
 qwen_infer.qwen_model_init()
+
 
 def load_and_transform_image(image_file: UploadFile):
     """Load an image file, transform it, and save as a tensor."""
     image = Image.open(image_file.file).convert("RGB")
-    transform = transforms.Compose([
-        transforms.Resize((448, 448)),  # Resize image to expected dimensions
-        transforms.ToTensor(),  # Convert to tensor
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # Normalize
-    ])
+    transform = transforms.Compose(
+        [
+            transforms.Resize((448, 448)),  # Resize image to expected dimensions
+            transforms.ToTensor(),  # Convert to tensor
+            transforms.Normalize(
+                mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+            ),  # Normalize
+        ]
+    )
     image_tensor = transform(image).unsqueeze(0)  # Add batch dimension
-    print(f'image_tensor {image_tensor.shape}')
+    print(f"image_tensor {image_tensor.shape}")
     # Save the tensor for inference
-    tensor_file_path = os.path.join(TENSOR_DIR, f'{image_file.filename}.pt')
-    
+    tensor_file_path = os.path.join(TENSOR_DIR, f"{image_file.filename}.pt")
+
     # file = f'{image_file.filename}.pt'
     torch.save(image_tensor, tensor_file_path)
-    
+
     return tensor_file_path
+
 
 @app.post("/infer/")
 async def infer(
     image: UploadFile = File(...),
     input_text: str = Form(...),
     max_new_tokens: int = Form(1024),
-    history: Optional[List[str]] = Form(None)
+    history: Optional[List[str]] = Form(None),
 ):
     try:
         temp_image_path = os.path.join(temp_dir, image.filename)
-        with open(temp_image_path, 'wb') as f:
-                contents = await image.read()
-                f.write(contents)
+        with open(temp_image_path, "wb") as f:
+            contents = await image.read()
+            f.write(contents)
         print(input_text)
         print(type(input_text))
         transformed_img_path = load_and_transform_image(image)
-        images = [{'image': transformed_img_path}]
+        images = [{"image": transformed_img_path}]
         stream = torch.cuda.current_stream().cuda_stream
         image_embeds = vit_process(images, vit_engine_dir, stream)
         print(temp_image_path)
         history = []
         content_list = images
-        content_list.append({'text': input_text})
+        content_list.append({"text": input_text})
         print("content list")
         output_text = qwen_infer.qwen_infer(
             input_vit=image_embeds,
             images_path=images,
             input_text=input_text,
             max_new_tokens=max_new_tokens,
-            history=history
+            history=history,
         )
         print(output_text)
         os.remove(temp_image_path)
@@ -78,5 +84,7 @@ async def infer(
         return {"output_text": output_text}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=80)
