0035fc323bb29774529e21fde4d3c7dc7fc8e366
Eternal Reclaimer <98760976+kyegomez@users.noreply.github.com>
2024-03-12 00:42:08 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,16 @@ provider "aws" {
   region = "us-east-1"
 }
 
+terraform {
+  backend "remote" {
+    organization = "swarms"
+
+    workspaces {
+      name = "swarms"
+    }
+  }
+}
+
 resource "aws_key_pair" "ssh_key" {
   key_name   = "ec2_ssh_key"
   public_key = file("cog_rsa.pub") # path to your public key
