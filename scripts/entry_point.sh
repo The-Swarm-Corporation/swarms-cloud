7df715e0cd61e17ca816a06c61de547a9488eb47
Kye <kye@apacmediasolutions.com>
2024-03-18 04:28:46 -0700

    [DOCKER][CLEANUP]

---
+++ scripts/entry_point.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Default values for environment variables
+export EVAL_PORT=${EVAL_PORT:-8000}
+export SERVER=${SERVER:-"http://localhost:${EVAL_PORT}"}
+export USE_GPU=${USE_GPU:-True}
+export WORLD_SIZE=${WORLD_SIZE:-4}
+export PSG_CONNECTION_STRING=${PSG_CONNECTION_STRING:-""}
+export COGVLM_MODEL_PATH=${COGVLM_MODEL_PATH:-"THUDM/cogvlm-chat-hf"}
+export SUPABASE_URL=${SUPABASE_URL:-""}
+export SUPABASE_KEY=${SUPABASE_KEY:-""}
+export HF_HUB_ENABLE_HF_TRANSFER=${HF_HUB_ENABLE_HF_TRANSFER:-True}
+
+# Run the application
+exec python3.10 -m uvicorn cogvlm:app --host 0.0.0.0 --port $EVAL_PORT
