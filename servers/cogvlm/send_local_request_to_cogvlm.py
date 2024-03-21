cf09323c9827e477aebb6fa9ddefb9494745d875
Kye <kye@apacmediasolutions.com>
2024-03-20 17:01:10 -0700

    [LOCAL REQUEST]

--- servers/cogvlm/send_local_request_to_cogvlm.py
+++ servers/cogvlm/send_local_request_to_cogvlm.py
@@ -31,7 +31,7 @@ request_data = {
 }
 
 # Specify the URL of your FastAPI application
-url = "http://api.swarms.world/v1/chat/completions"
+url = "http://localhost:8000/v1/chat/completions"
 
 # Send the request
 response = requests.post(url, json=request_data)
