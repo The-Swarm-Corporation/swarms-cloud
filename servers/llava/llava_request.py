d9d8d07828a12a79c29792479ff4eaeb162a9297
Zack <zack@zackbradshaw.com>
2024-04-22 09:08:54 -0500

    feat: add llava serve, and request

--- servers/llava/llava_request.py
+++ servers/llava/llava_request.py
@@ -1,14 +1,24 @@
-llm = LLM(
-    model="llava-hf/llava-1.5-7b-hf",
-    image_input_type="pixel_values",
-    image_token_id=32000,
-    image_input_shape="1,3,336,336",
-    image_feature_size=576,
-)
-
-prompt = "<image>" * 576 + "What is the content of this image?"
-
-images=torch.load("xxx")  # This should be generated offline or by another online component. See tests/images/ for samples
-
-from vllm.sequence import MultiModalData
-llm.generate(prompt, multi_modal_data=MultiModalData(type=MultiModalData.Type.IMAGE, data=images))
+    from openai import OpenAI
+    openai_api_key = "EMPTY"
+    openai_api_base = "http://localhost:8000/v1"
+    client = OpenAI(
+        api_key=openai_api_key,
+        base_url=openai_api_base,
+    )
+    # Note that this model expects the image to come before the main text
+    chat_response = client.chat.completions.create(
+        model="llava-hf/llava-1.5-7b-hf",
+        messages=[{
+            "role": "user",
+            "content": [
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+                    },
+                },
+                {"type": "text", "text": "What's in this image?"},
+            ],
+        }],
+    )
+    print("Chat response:", chat_response)
