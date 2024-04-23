99c927330f2f2cf477fc2793882047b1c19687cc
Zack <zack@zackbradshaw.com>
2024-04-23 01:22:08 -0500

    refactor: refactor to lm_deploy yaml

--- servers/llava/llava_request.py
+++ servers/llava/llava_request.py
@@ -1,24 +1,24 @@
-    from openai import OpenAI
-    openai_api_key = "EMPTY"
-    openai_api_base = "http://localhost:8000/v1"
-    client = OpenAI(
-        api_key=openai_api_key,
-        base_url=openai_api_base,
-    )
-    # Note that this model expects the image to come before the main text
-    chat_response = client.chat.completions.create(
-        model="llava-hf/llava-1.5-7b-hf",
-        messages=[{
-            "role": "user",
-            "content": [
-                {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
-                    },
+from openai import OpenAI
+openai_api_key = "EMPTY"
+openai_api_base = "http://localhost:8000/v1"
+client = OpenAI(
+    api_key=openai_api_key,
+    base_url=openai_api_base,
+)
+# Note that this model expects the image to come before the main text
+chat_response = client.chat.completions.create(
+    model="llava-hf/llava-1.5-7b-hf",
+    messages=[{
+        "role": "user",
+        "content": [
+            {
+                "type": "image_url",
+                "image_url": {
+                    "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
                 },
-                {"type": "text", "text": "What's in this image?"},
-            ],
-        }],
-    )
-    print("Chat response:", chat_response)
+            },
+            {"type": "text", "text": "What's in this image?"},
+        ],
+    }],
+)
+print("Chat response:", chat_response)
