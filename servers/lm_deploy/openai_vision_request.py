c8f9bd6f093c4310c1778fda3c5ae7b84813fa79
Zack <zack@zackbradshaw.com>
2024-04-30 17:30:31 -0500

    fix: merge issues

---
+++ servers/lm_deploy/openai_vision_request.py
@@ -0,0 +1,33 @@
+from openai import OpenAI
+
+import os
+
+# openai_api_key = os.getenv("OPENAI_API_KEY")
+# openai_api_base = os.getenv("OPENAI_API_BASE", "https://api.openai.com/v1")
+# model = os.getenv("MODEL")
+
+openai_api_key = ""
+openai_api_base = "http://44.211.226.148:30001/v1"
+model = "llava"
+
+client = OpenAI(
+    api_key=openai_api_key,
+    base_url=openai_api_base,
+)
+# Note that this model expects the image to come before the main text
+chat_response = client.chat.completions.create(
+    model=model,
+    messages=[{
+        "role": "user",
+        "content": [
+            {
+                "type": "image_url",
+                "image_url": {
+                    "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+                },
+            },
+            {"type": "text", "text": "What's in this image?"},
+        ],
+    }],
+)
+print("Chat response:", chat_response)
