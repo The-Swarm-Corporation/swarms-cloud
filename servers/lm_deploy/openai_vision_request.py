9b0e38e2fc5a8b12266730bfab161ea874935546
Zack <zack@zackbradshaw.com>
2024-04-30 21:30:04 -0500

    feat: refactor and cleanup yamls

--- servers/lm_deploy/openai_vision_request.py
+++ servers/lm_deploy/openai_vision_request.py
@@ -1,33 +1,35 @@
 from openai import OpenAI
-
+from dotenv import load_dotenv
 import os
 
-# openai_api_key = os.getenv("OPENAI_API_KEY")
-# openai_api_base = os.getenv("OPENAI_API_BASE", "https://api.openai.com/v1")
-# model = os.getenv("MODEL")
-
-openai_api_key = ""
-openai_api_base = "http://44.211.226.148:30001/v1"
-model = "llava"
+# Load environment variables from .env file
+load_dotenv()
+openai_api_base = os.getenv("OPENAI_API_BASE")
+openai_api_key = os.getenv("OPENAI_API_KEY")
+model = os.getenv("MODEL")
 
 client = OpenAI(
     api_key=openai_api_key,
     base_url=openai_api_base,
 )
-# Note that this model expects the image to come before the main text
-chat_response = client.chat.completions.create(
+
+try:
+    chat_response = client.chat.completions.create(
     model=model,
-    messages=[{
-        "role": "user",
-        "content": [
-            {
+    messages=[
+        {
+            "role": "user",
+            "content": {
                 "type": "image_url",
-                "image_url": {
-                    "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
-                },
+                "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
             },
-            {"type": "text", "text": "What's in this image?"},
-        ],
-    }],
-)
-print("Chat response:", chat_response)
+            "role": "user",
+            "content": {
+                "type": "text",
+                "text": "What is in the image "
+            }
+        }],
+    )
+    print("Chat response:", chat_response)
+except Exception as e:
+    print(f"An error occurred: {e}")
