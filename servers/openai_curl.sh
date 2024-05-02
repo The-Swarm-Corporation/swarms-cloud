6cd7ccb3a6291f2449d7a795337851452ee9b8e5
Zack <zack@zackbradshaw.com>
2024-05-01 19:07:57 -0500

    Add new openai_curl script

---
+++ servers/openai_curl.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Load environment variables from .env file
+if [ -f .env ]; then
+  export $(cat .env | xargs)
+fi
+
+# Get the model from environment variable
+MODEL=$MODEL
+
+# First run curl on v1/models to get available models
+curl http://$OPENAI_API_BASE:8080/v1/models
+
+# Run curl on v1/chat/completions with the specified model
+curl http://$OPENAI_API_BASE/v1/chat/completions \
+  -H "Content-Type: application/json" \
+  -d '{
+    "model": "'$MODEL'",
+    "messages": [{"role": "user", "content": "Hello! How are you?"}]
+  }'
