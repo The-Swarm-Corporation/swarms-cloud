dc5a119c56c84a2e059e005b3e74368dabfb8040
Kye <kye@apacmediasolutions.com>
2024-05-16 10:22:07 -0700

    [CLEANUP]

--- swarms_cloud/check_model_list.py
+++ swarms_cloud/check_model_list.py
@@ -1,21 +1,10 @@
 from http import HTTPStatus
-from typing import Optional
 
 from fastapi.responses import JSONResponse
 
 from swarms_cloud.schema.openai_protocol import (  # noqa: E501
     ErrorResponse,
 )
-
-
-def get_model_list():
-    """Available models."""
-    # Use the model registry from swarms
-    return [
-        "Qwen/Qwen-VL-Chat-Int4",
-        "Qwen/Qwen-VL-Chat",
-        "Qwen/Qwen-VL",
-    ]
 
 
 def create_error_response(status: HTTPStatus, message: str):
@@ -30,13 +19,3 @@ def create_error_response(status: HTTPStatus, message: str):
             message=message, type="invalid_request_error", code=status.value
         ).model_dump()
     )
-
-
-async def check_request(request) -> Optional[JSONResponse]:
-    """Check if a request is valid."""
-    if request.model in get_model_list():
-        return
-    ret = create_error_response(
-        HTTPStatus.NOT_FOUND, f"The model `{request.model}` does not exist."
-    )
-    return ret
