7ff2d6077ad7b80f55c04402f7545f0a8e79f36b
Kye Gomez <swarms_wd@Kyes-MacBook-Pro.local>
2024-06-27 12:45:22 -0700

    [CLEANUP]

--- servers/cogvlm/openai_api_example.py
+++ servers/cogvlm/openai_api_example.py
@@ -3,10 +3,10 @@ import openai
 # Set the API key
 client = openai.OpenAI(
     #  clusters
-    # base_url="https://44.203.144.168:30001/v1", api_key="sk-1g3Z3y2c1f2z3d4b5p6w7c8b9v0n1m2"
+    # base_url="https://44.203.144.168:30001/v1", api_key=""
     base_url="http://40.87.83.103:30001/v1",
     api_key="",
-    # base_url="https://18.189.185.191:8000/v1", api_key="sk-1g3Z3y2c1f2z3d4b5p6w7c8b9v0n1m2"
+    # base_url="https://18.189.185.191:8000/v1", api_key=""
 )
 # Create a client object to interact with the OpenAI API
 # Make a chat completion request
